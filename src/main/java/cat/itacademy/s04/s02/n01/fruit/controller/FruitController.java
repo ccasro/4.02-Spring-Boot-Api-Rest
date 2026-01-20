3c80583c4e16c4c32e310682815f53ad3b50ed02
Cesar <ccasro@gmail.com>
2026-01-20 16:55:30 +0100

    chore(FruitController): add controller skeleton and record DTO

---
+++ src/main/java/cat/itacademy/s04/s02/n01/fruit/controller/FruitController.java
@@ -0,0 +1,20 @@
+package cat.itacademy.s04.s02.n01.fruit.controller;
+
+
+import cat.itacademy.s04.s02.n01.fruit.dto.FruitRequestDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fruits")
+public class FruitController {
+
+    @PostMapping
+    public ResponseEntity<Void> createFruit(@Valid @RequestBody FruitRequestDTO request) {
+        return ResponseEntity.status(500).build();
+    }
+}
---
+++ src/main/java/cat/itacademy/s04/s02/n01/fruit/dto/FruitRequestDTO.java
@@ -0,0 +1,5 @@
+package cat.itacademy.s04.s02.n01.fruit.dto;
+
+
+public record FruitRequestDTO(  String name, Integer weightInKilos) {
+}
